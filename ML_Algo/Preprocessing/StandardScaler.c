e9c2dbfd28dff28842df54a140e42dcd676e7447
Ashish Shevale <shevaleashish@gmail.com>
2020-09-27 17:14:36 +0530

    Added fit_transform method to the Standard Scaler

--- ML_Algo/Preprocessing/StandardScaler.c
+++ ML_Algo/Preprocessing/StandardScaler.c
@@ -105,12 +105,72 @@ PyObject* transform( PyObject* self, PyObject* args)
     return PyArray_Return( output );
 }
 
+PyObject* fit_transform( PyObject* self, PyObject* args )
+{
+    PyArrayObject* input_array = NULL;
+    // Parse the passed arguments and store the results in range_min and range_max variables
+    if( ! PyArg_ParseTuple( args, "O", &input_array ) )
+        return NULL;
+
+    // Convert the data type of input_array so, the function becomes universal for all dtype arrays
+    input_array = (PyArrayObject*) PyArray_Cast(input_array, NPY_FLOAT);
+    
+    // Store the dimensions of the input array
+    int n_dims = PyArray_NDIM( input_array );
+    
+    // This functions will only work on single column arrays, warn the user about it
+    if( n_dims != 1 )
+        PyErr_SetString( PyExc_ValueError, "StandardScaler currently supports single column data only." );
+    
+    // Get the number of rows in the data
+    int rows = (int) PyArray_SHAPE( input_array )[0];
+    
+    // Get the pointer to the data inside the numpy array
+    float* data = (float*) PyArray_DATA( input_array );
+    
+    // Initialize the data_mean and data_std to zero.
+    data_mean = 0, data_std = 0;
+    
+    // From the data, calculate the mean of all elements
+    for(int i = 0; i < rows; ++i)
+        data_mean += data[i];
+        
+    // data_mean currently holds the sum of elements of data,
+    // So divide it by number of elements to get the mean
+    data_mean /= rows;
+    
+    // Using the data elements and the mean, calculate the standard deviation
+    for(int i = 0; i < rows; ++i)
+        data_std += ( data[i] - data_mean ) * ( data[i] - data_mean );
+        
+    // data_std holds the sum of square of difference of elements and the mean
+    // divide it by no of elements and take square root to get the standard devaition
+    data_std = sqrt( data_std / rows );
+
+    // Create a pointer to store the result and assign memory to it
+    float* out_data = (float*) malloc( rows * sizeof(float) );
+    
+    // Go through every element of data
+    for( int i = 0; i < rows; ++i )
+    {
+        // Apply the StandardScaler formula to every element
+        out_data[ i ] = ( data[ i ] - data_mean ) / data_std;
+    }
+    
+    // Create a new Python Array Object that stores the transformed data
+    PyArrayObject* output = (PyArrayObject*) PyArray_SimpleNewFromData(1, PyArray_DIMS( input_array ), NPY_FLOAT, (void*) out_data);
+    
+    // Return the Python-Numpy Array Object
+    return PyArray_Return( output );
+}
+
 //########        MODULE LEVEL FUNCTIONS        ########
 
 // method definitions
 static PyMethodDef methods[] = {
   { "fit", fit, METH_VARARGS, "Fits the StandardScaler on the data given to it"},
   { "transform", transform, METH_VARARGS, "Transforms the data as per the StandardScaler formula"},
+  {"fit_transform", fit_transform, METH_VARARGS, "Fits the Standard Scaler and then transform the data as per the Standard Scaler formula"},
   { NULL, NULL, 0, NULL }
 };
 
