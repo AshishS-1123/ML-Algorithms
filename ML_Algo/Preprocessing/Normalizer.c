41322530512fae16bd621088850886c4c3cf63ec
Ashish Shevale <shevaleashish@gmail.com>
2020-09-29 22:49:26 +0530

    Added Normalizer (Norm L1) to Preprocessing

--- ML_Algo/Preprocessing/Normalizer.c
+++ ML_Algo/Preprocessing/Normalizer.c
@@ -68,21 +68,100 @@ PyObject* norm_l2( PyObject* self, PyObject* args )
         {
             out[ i * shape[1] + j ] = data[ i * shape[1] + j ] / norm;
         }
-        
     }
     
     // Create a Python-NumPy array object to store the transformed output
-        PyArrayObject* output = (PyArrayObject*) PyArray_SimpleNewFromData(2, shape, NPY_FLOAT, (void*) out );
+    PyArrayObject* output = (PyArrayObject*) PyArray_SimpleNewFromData(2, shape, NPY_FLOAT, (void*) out );
         
-        return PyArray_Return( output );
+    return PyArray_Return( output );
         
 }
+
+PyObject* norm_l1( PyObject* self, PyObject* args )
+{
+    // Create a NumPy artay object to hold the input array
+    PyArrayObject* input_array = NULL;
+    
+    // Parse the input arguments and get the input array
+    if( ! PyArg_ParseTuple(args, "O", &input_array ) )
+        return NULL;
+    
+    // Cast the input_array to dtype float so the function can now be used for all data types
+    input_array = (PyArrayObject*) PyArray_Cast(input_array, NPY_FLOAT);
+    
+    // Get the dimensions of the array object and store it
+    npy_intp ndims = PyArray_NDIM( input_array );
+    
+    // Check whether the dimensions provided by the user are of correct dimensions
+    switch( ndims )
+    {
+        // If the user provided a scaler or empty array, inform the user through error
+        case 0: PyErr_SetString( PyExc_ValueError, "Cannot process empty data");
+                      break;
+        // if the user provided a 1d array, it does not need normalising as we need at lest 2 features
+        case 1: PyErr_SetString( PyExc_ValueError, "Data with only 1 observation does no need to be normalized");
+                      break;
+        // If the user provided a 2d array, its ok
+        case 2: break;
+        // If the user provided array with any other dimension, inform user through error
+        default: PyErr_SetString( PyExc_ValueError, "Data must be 2 dimensional only");
+    }
+    
+    // Create a variable to hold the number of rows in the array
+    npy_intp* shape = PyArray_SHAPE( input_array );
+    
+    // Create a pointer and point it to the data of the array
+    float* data = (float*) PyArray_DATA( input_array );
+    
+    // Create a pointr to store the transformed data and assign space to it
+    float* out = (float*) malloc( shape[0] * shape[1] * sizeof( float ) );
+    
+    // Repeat the following for every observation in the data
+    for( int i = 0; i < shape[0]; ++i )
+    {
+        // STEP 1 : : CALCULATE THE  NORM i.e. sqrt( x1 ^ 2 + x2 ^ 2 ... xn ^ 2 )
+        
+        // Create a variable to hold the norm
+        float norm = 0;
+        
+        // Loop through every element and add its square to norm
+        for( int j = 0; j < shape[1]; ++j )
+        {
+            // Store the value of current element in a temporary variable
+            float temp = data[ i * shape[1] + j ];
+            
+            // If the element is positive,
+            if( temp > 0 )
+                // Add it to the norm
+                norm += data[ i * shape[1] + j ];
+            // If the element is negative,
+            else
+                // Subtract it from the norm. So we are effectively adding its magnitude to norm
+                norm -= data[ i * shape[1] + j ];
+        }
+        
+        // STEP 2 : : TRANSFORM THE DATA ACCORDING TO THE NORM
+        
+        // Loop through every element of observation and transform it
+        for(int j = 0; j < shape[1]; ++j)
+        {
+            out[ i * shape[1] + j ] = data[ i * shape[1] + j ] / norm;
+        }
+    }
+    
+    // Create a Python-NumPy array object to store the transformed output
+     PyArrayObject* output = (PyArrayObject*) PyArray_SimpleNewFromData(2, shape, NPY_FLOAT, (void*) out );
+        
+    return PyArray_Return( output );        
+}
+
 
 //########        MODULE LEVEL FUNCTIONS        ########
 
 // method definitions
 static PyMethodDef methods[] = {
   {"NormL2", norm_l2, METH_VARARGS, "Normalizes observations with L2 Norm"},
+  {"NormL1", norm_l1, METH_VARARGS, "Normalizes observations with L1 Norm"},
   { NULL, NULL, 0, NULL }
 };
 
