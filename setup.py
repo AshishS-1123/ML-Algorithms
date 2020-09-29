e61d419124015e458a5de4015eeaf9fd35b29109
Ashish Shevale <shevaleashish@gmail.com>
2020-09-29 22:40:06 +0530

    Added Normalizer extension to setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,14 @@ PrePro_MinMax_Extension = Extension(name = "ML_Algo.Preprocessing.MinMaxScaler",
 # Standard Scaler Extension in Preprocessing
 PrePro_Std_Extension = Extension(name = "ML_Algo.Preprocessing.StandardScaler", sources = ["ML_Algo/Preprocessing/StandardScaler.c"])
 
+# Normalizer Extension in Preprocessing
+Norm_Extension = Extension(name = "ML_Algo.Preprocessing.Normalizer", sources = ["ML_Algo/Preprocessing/Normalizer.c"])
+
 # Create a list containing all the extensions
 ext_modules = [
                             PrePro_MinMax_Extension,
-                            PrePro_Std_Extension
+                            PrePro_Std_Extension,
+                            Norm_Extension
 ]
 
 ####################### SETUP FOR PACKAGE #######################
